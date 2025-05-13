cd5112ed93179c9a77e22279b950db63d052282c
kenanalic20 <kenan.alic@edu.fit.ba>
2025-05-14 00:01:22 +0200

    Fixed consumer

--- MilkMaster/MilkMaster.Subscriber/Program.cs
+++ MilkMaster/MilkMaster.Subscriber/Program.cs
@@ -1,9 +1,9 @@
 ﻿using MilkMaster.Application.DTOs;
-using MilkMaster.Infrastructure.Services;
+using MilkMaster.Subscriber.Service;
 
 Console.WriteLine("Starting RabbitMQ Consumer...");
 var consumer = new RabbitMqConsumerService();
 await consumer.StartListening<SettingsDto>();
 
-Console.WriteLine("Press any key to exit...");
+
 Console.ReadKey();
