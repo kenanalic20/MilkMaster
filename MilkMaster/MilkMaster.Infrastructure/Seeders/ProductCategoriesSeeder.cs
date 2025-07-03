9ab29a2f363656bd393661cf3e94571e37031983
kenanalic20 <kenan.alic@edu.fit.ba>
2025-07-03 02:32:30 +0200

    Desktop login and few fixes on backend

--- MilkMaster/MilkMaster.Infrastructure/Seeders/ProductCategoriesSeeder.cs
+++ MilkMaster/MilkMaster.Infrastructure/Seeders/ProductCategoriesSeeder.cs
@@ -1,7 +1,17 @@
 ﻿
+
+using MilkMaster.Application.Interfaces.Services;
+
 namespace MilkMaster.Infrastructure.Seeders
 {
     public class ProductCategoriesSeeder
     {
+        private readonly IProductCategoriesService _productCategoriesService;
+
+        public ProductCategoriesSeeder(IProductCategoriesService productCategoriesService)
+        {
+            _productCategoriesService = productCategoriesService;
+        }   
+
     }
 }
---
+++ milkmaster_desktop/android/app/src/main/kotlin/com/example/milkmaster_desktop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.milkmaster_desktop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
